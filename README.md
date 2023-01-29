0a338ed8a0a3356a75e40f235d539769f062e627
katavnnn <111687886+katavnnn@users.noreply.github.com>
2023-01-29 13:28:51 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+a music player web with many V-pop songs 
+![Alt text]( https://scontent.fhan4-3.fna.fbcdn.net/v/t1.15752-9/328031539_909034273464831_6975808229105945214_n.png?_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=WTvmQJX184AAX-KgA1U&_nc_ht=scontent.fhan4-3.fna&oh=03_AdS4gSvZx2AqhWzqQJbvB9xGmT1U8ABvz2pvCWpTl7uouA&oe=63FD6D18 "Optional title")
---
+++ app.js
@@ -0,0 +1,638 @@
+let  allMusic = [
+    {
+        name: "ngủ một mình (tình rất tình)",
+        artist: "HIEUTHUHAI, Negav, Kewtiie",
+        img: "nmm",
+        src: "music/1.mp3",
+        idMusic: "song1"
+    },
+    {
+        name: "Chuyện Đôi Ta (feat. Muộii)",
+        artist: "Emcee L (Da LAB), Muộii (Starry Night)",
+        img: "cdt",
+        src: "music/2.mp3",
+        idMusic: "song2"
+    },
+    {
+        name: " Anh Chưa Thương Em Đến Vậy Đâu",
+        artist: "Myra Trần",
+        img: "actedvd",
+        src: "music/4.mp3",
+        idMusic: "song3"
+    },
+    {
+        name: "tình yêu chậm trễ",
+        artist: "MONSTAR",
+        img: "tyct",
+        src: "music/3.mp3",
+        idMusic: "song4"
+    },
+    {
+        name: " vaicaunoicokhiennguoithaydoi",
+        artist: "GREY D, tlinh",
+        img: "vcn",
+        src: "music/5.mp3",
+        idMusic: "song5"
+    },
+    {
+        name: "Vì Anh Đâu Có Biết",
+        artist: "Madihu, Vũ.",
+        img: "vadcb",
+        src: "music/6.mp3",
+        idMusic: "song6"
+    },
+    {
+        name: "dự báo thời tiết hôm nay mưa",
+        artist: "GREY D",
+        img: "dbtthnm",
+        src: "music/7.mp3",
+        idMusic: "song7"
+    },
+    {
+        name: "Anh Đánh Rơi Người Yêu Này",
+        artist: "- Andiez, AMEE ",
+        img: "adrnyn",
+        src: "music/8.mp3",
+        idMusic: "song8"
+    },
+    {
+        name: "Sài Gòn Hôm Nay Mưa",
+        artist: "JSOL, Hoàng Duyên",
+        img: "sghnm",
+        src: "music/9.mp3",
+        idMusic: "song9"
+    },
+    {
+        name: "có hẹn với thanh xuân",
+        artist: "MONSTAR, GREY D",
+        img: "chvtx",
+        src: "music/10.mp3",
+        idMusic: "song10"
+    },
+    {
+        name: "Em Không Hiểu",
+        artist: "Changg, Minh Huy",
+        img: "ekh",
+        src: "music/11.mp3",
+        idMusic: "song11"
+    },
+    
+    {
+        name: "Có Em (feat. Low G",
+        artist: "Có Em (feat. Low G",
+        img: "ce",
+        src: "music/12.mp3",
+        idMusic: "song12"
+    },
+    
+    {
+        name: "Một Ngàn Nỗi Đau",
+        artist: "Văn Mai Hương, Hứa Kim Tuyền",
+        img: "mnnd",
+        src: "music/13.mp3",
+        idMusic: "song13"
+    },
+    
+    {
+        name: "tiny love",
+        artist: "Thịnh Suy",
+        img: "tl",
+        src: "music/14.mp3",
+        idMusic: "song14"
+    },
+    
+    {
+        name: "Thích Quá Rùi Nà",
+        artist: "tlinh, Trung Trần",
+        img: "tqrn",
+        src: "music/15.mp3",
+        idMusic: "song15"
+    },
+    
+    {
+        name: "Lỡ Say Bye Là Bye",
+        artist: "Changg, Lemese",
+        img: "lsblb",
+        src: "music/16.mp3",
+        idMusic: "song16"
+    },
+    
+    {
+        name: "Có Đâu Ai Ngờ",
+        artist: "Cầm",
+        img: "cdan",
+        src: "music/17.mp3",
+        idMusic: "song17"
+    },
+    
+    {
+        name: "Ai Đưa Em Về",
+        artist: "TIA, Le Thien Hieu",
+        img: "adev",
+        src: "music/18.mp3",
+        idMusic: "song18"
+    },
+    {
+        name: "Thanh Xuan",
+        artist: "Da LAB",
+        img: "tx",
+        src: "music/19.mp3",
+        idMusic: "song19"
+    },
+    {
+        name: "Thức Giắc",
+        artist: "Da LAB",
+        img: "tg",
+        src: "music/20.mp3",
+        idMusic: "song20"
+    },
+    {
+        name: "Bo Xì Bo",
+        artist: "Hoang Thuy Linh",
+        img: "bxb",
+        src: "music/21.mp3",
+        idMusic: "song21"
+    },
+    {
+        name: "Waiting For You",
+        artist: "MONO",
+        img: "wfy",
+        src: "music/22.mp3",
+        idMusic: "song22"
+    },
+    {
+        name: "Muốn Nói Với Em",
+        artist: "T Team",
+        img: "mnve",
+        src: "music/23.mp3",
+        idMusic: "song23"
+    },
+    {
+        name: "Ngày Đầu Tiên",
+        artist: "Đức Phúc",
+        img: "ndt",
+        src: "music/24.mp3",
+        idMusic: "song24"
+    },
+    {
+        name: "Em Day Chang Phai Thuy Kieu",
+        artist: "Hoang Thuy Linh",
+        img: "edcptk",
+        src: "music/25.mp3",
+        idMusic: "song25"
+    },
+    {
+        name: "Dù Cho Mai Về Sau",
+        artist: "buitruonglinh",
+        img: "26",
+        src: "music/26.mp3",
+        idMusic: "song26"
+    },
+    {
+        name: "Chạy Khỏi Thế Giới Này",
+        artist: "Da LAB, Phuong Ly",
+        img: "cktgn",
+        src: "music/27.mp3",
+        idMusic: "song27"
+    },
+    {
+        name: "Ánh Sao Và Bầu Trời",
+        artist: "T.R.I",
+        img: "asvbt",
+        src: "music/28.mp3",
+        idMusic: "song28"
+    },
+    {
+        name: "bao tiền một mớ bình yên",
+        artist: "14 Casper, Bon Nghiêm",
+        img: "btmmby",
+        src: "music/29.mp3",
+        idMusic: "song29"
+    },
+    {
+        name: "Yêu Một Người Có Lẽ",
+        artist: "Lou Hoang, Miu Lê",
+        img: "ymncl",
+        src: "music/30.mp3",
+        idMusic: "song30"
+    },
+    {
+        name: "Tình Ca Tình Ta",
+        artist: "kis",
+        img: "tcctt",
+        src: "music/31.mp3",
+        idMusic: "song31"
+    },
+    {
+        name: "Gác Lại Âu Lo",
+        artist: "Da LAB, Miu Lê",
+        img: "glal",
+        src: "music/32.mp3",
+        idMusic: "song32"
+    },
+    {
+        name: "Liệu Giờ",
+        artist: "2T, Văn ",
+        img: "lq",
+        src: "music/33.mp3",
+        idMusic: "song33"
+    },
+    {
+        name: "Em là",
+        artist: "MONO, Onionn.",
+        img: "el",
+        src: "music/34.mp3",
+        idMusic: "song"
+    },
+    {
+        name: "Lâu Lâu Nhắc Lại",
+        artist: "Ha Nhi, Khói",
+        img: "llnl",
+        src: "music/35.mp3",
+        idMusic: "song35"
+    },
+    {
+        name: "Và Ngày Nào Đó",
+        artist: "Trung Quân",
+        img: "vnnd",
+        src: "music/36.mp3",
+        idMusic: "song36"
+    },
+    {
+        name: "Sinh Ra Đã Là Thứ Đối Lập Nhau (feat. Badbies)",
+        artist: "Emcee L (Da LAB), Da LAB, Badbies",
+        img: "srdltdln",
+        src: "music/37.mp3",
+        idMusic: "song37"
+    },
+    {
+        name: "Chết Trong Em",
+        artist: "Thịnh Suy",
+        img: "cte",
+        src: "music/38.mp3",
+        idMusic: "song38"
+    },
+    {
+        name: "Cho Ngày Không Còn Nhau",
+        artist: "T.R.I",
+        img: "cnkcn",
+        src: "music/39.mp3",
+        idMusic: "song39"
+    },
+    {
+        name: "Real Love - The Heroes Version",
+        artist: "My Anh, Khắc Hưng",
+        img: "rl",
+        src: "music/40.mp3",
+        idMusic: "song40"
+    },
+    {
+        name: "ThichThich",
+        artist: "Phuong Ly",
+        img: "41",
+        src: "music/41.mp3",
+        idMusic: "song41"
+    },
+    {
+        name: "Hình Trái Tim",
+        artist: "Thắng, Vũ Thanh Vân",
+        img: "htt",
+        src: "music/42.mp3",
+        idMusic: "song42"
+    },
+    {
+        name: "Chillies",
+        artist: "Mascara",
+        img: "m",
+        src: "music/43.mp3",
+        idMusic: "song43"
+    },
+    {
+        name: "See Tình",
+        artist: "Hoang Thuy Linh",
+        img: "st",
+        src: "music/44.mp3",
+        idMusic: "song44"
+    },
+    {
+        name: "RPT Orijinn, Ronboogz",
+        artist: "don't côi",
+        img: "dc",
+        src: "music/45.mp3",
+        idMusic: "song45"
+    },
+    {
+        name: "Khi Em Lớn",
+        artist: "Orange, Hoàng Dũng",
+        img: "kel",
+        src: "music/46.mp3",
+        idMusic: "song46"
+    },
+    {
+        name: "Chúng Ta Sau Này",
+        artist: "T.R.I",
+        img: "ctsn",
+        src: "music/47.mp3",
+        idMusic: "song47"
+    },
+    {
+        name: "Lạ Lùng",
+        artist: "Vũ.",
+        img: "ll",
+        src: "music/48.mp3",
+        idMusic: "song48"
+    },
+    {
+        name: "Ngã Tư Không Đèn",
+        artist: "TRANG, Khoa Vũ",
+        img: "ntkd",
+        src: "music/49.mp3",
+        idMusic: "song49"
+    },
+    {
+        name: "Thói Quen (25 Mét Vuông)",
+        artist: "Hoàng Dũng, GDucky",
+        img: "tq",
+        src: "music/50.mp3",
+        idMusic: "song50"
+    },
+    {
+        name: "Tự Tình 2",
+        artist: " Trung Quân",
+        img: "tt2",
+        src: "music/51.mp3",
+        idMusic: "song51"
+    },
+    {
+        name: "Tình Yêu Đến Sau",
+        artist: "Myra Trần",
+        img: "tyds",
+        src: "music/52.mp3",
+        idMusic: "song52"
+    },
+    {
+        name: "Đường Tôi Chở Em Về",
+        artist: "buitruonglinh",
+        img: "dtcev",
+        src: "music/53.mp3",
+        idMusic: "song53"
+    },
+    {
+        name: "Anh Nhớ Ra (feat. TRANG)",
+        artist: "Vũ., TRANG",
+        img: "anr",
+        src: "music/54.mp3",
+        idMusic: "song54"
+    },
+    {
+        name: "3107 3.",
+        artist: "W N, Duongg, Nau, titie",
+        img: "31073",
+        src: "music/55.mp3",
+        idMusic: "song55"
+    },
+    {
+        name: "Thích Em Hơi Nhiều",
+        artist: "Wren Evans",
+        img: "tehn",
+        src: "music/56.mp3",
+        idMusic: "song56"
+    },
+    {
+        name: "Quá Khứ Đôi, Hiện Tại Đơn",
+        artist: "Đức Phúc",
+        img: "qkdhtd",
+        src: "music/57.mp3",
+        idMusic: "song57"
+    },
+    {
+        name: "Cà Phê",
+        artist: "MIN",
+        img: "cp",
+        src: "music/58.mp3",
+        idMusic: "song58"
+    },
+    
+    
+
+
+
+]
+
+const wrapper = document.querySelector(".wrapper")
+imgMusic = wrapper.querySelector('.img-music')
+musicImg = wrapper.querySelector(".img-music img")
+musicName = wrapper.querySelector(".name-music .name")
+musicArtist = wrapper.querySelector(".name-music .artist")
+progress = wrapper.querySelector('.progress')
+progressbar = wrapper.querySelector(".progress-bar")
+play = wrapper.querySelector('#play');
+prev = wrapper.querySelector('#prev');
+next = wrapper.querySelector('#next');
+
+let musicIndex = 0;
+let autoplay = 0;
+let timer = 0;
+let play_song = false;
+let songAudio = document.createElement('audio');
+
+window.addEventListener("load", ()=>{
+    loadMusic(musicIndex);
+    playLiMusic();
+})
+function loadMusic(index){
+    reset_music();
+    autoplay_song();
+    musicName.innerText = allMusic[index].name;
+    musicArtist.innerText = allMusic[index].artist;
+    musicImg.src = `image/${allMusic[index].img}.jpg`;
+    songAudio.src = allMusic[index].src;
+    songAudio.load();
+
+    timer = setInterval(range_slider , 1000);
+}
+loadMusic(musicIndex);
+function musicPlay(){
+    if(play_song == false){
+        playsong();
+    }
+    else{
+        pausesong();
+    }
+}
+function playsong(){
+    songAudio.play();
+    play_song = true;
+    play.innerHTML = '<i class="fa-solid fa-pause"></i>';
+}
+
+function pausesong(){
+    songAudio.pause();
+    play_song = false;
+    play.innerHTML = '<i class="fa-solid fa-play"></i>';
+}
+
+// ---------next Music----------
+function nextMusic(){
+    if(musicIndex < allMusic.length -1){
+        musicIndex++;
+        loadMusic(musicIndex);
+        playsong();
+        playLiMusic();
+    }else{
+        musicIndex = 0;
+        loadMusic(musicIndex);
+        playsong();
+        playLiMusic();
+    }
+}
+// ---------prev Music----------
+function prevMusic(){
+    if(musicIndex > 0){
+        musicIndex--;
+        loadMusic(musicIndex);
+        playsong();
+    }else{
+        musicIndex = allMusic.length -1;
+        loadMusic(musicIndex);
+        playsong();
+    }
+}
+next.addEventListener("click", ()=> {
+    nextMusic();
+})
+
+prev.addEventListener("click", ()=> {
+    prevMusic();
+})
+
+// -------------update progress-------
+songAudio.addEventListener("timeupdate", (e)=>{
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    let progressWidth = (currentTime/duration)*100;
+    progressbar.style.width = `${progressWidth}%`;
+
+    let musicCurrentTime = wrapper.querySelector(".current")
+    let musicDuration = wrapper.querySelector(".duration")
+    songAudio.addEventListener("loadeddata", ()=>{
+        let audioduration = songAudio.duration;
+        let totalMusicmin = Math.floor(audioduration/60);
+        let totalMusicsec = Math.floor(audioduration%60);
+        if(totalMusicsec < 10){
+            totalMusicsec = `0${totalMusicsec}`;
+        }
+        musicDuration.innerText = `${totalMusicmin}:${totalMusicsec}`;
+    });
+    let currentMusicmin = Math.floor(currentTime/60);
+    let currentMusicsec = Math.floor(currentTime%60);
+        if(currentMusicsec < 10){
+            currentMusicsec = `0${currentMusicsec}`;
+        }
+        musicCurrentTime.innerText = `${currentMusicmin}:${currentMusicsec}`;
+
+});
+
+// -----------tua nhạc-------
+progress.addEventListener("click", (e)=>{
+    let progressWidthval = progress.clientWidth;
+    let clickOffsetX = e.offsetX;
+    let songDuration = songAudio.duration;
+    songAudio.currentTime = (clickOffsetX / progressWidthval) * songDuration
+    playsong();
+})
+
+// --------tự chuyển bài----------
+function autoplay_song(){
+    if(autoplay==1){
+        autoplay=0;
+    }else{
+        autoplay=1;
+    }
+}
+function range_slider(){
+    if(songAudio.ended){
+        play.innerHTML = `<i class="fa-solid fa-play"></i>`;
+        if(autoplay==1){
+            nextMusic();
+            loadMusic(musicIndex);
+            playsong();
+        }
+    }
+}
+// --------reset music---------
+function reset_music(){
+    progressbar.value = 0;
+}
+
+// --------Volume--------
+
+let recent_volume = wrapper.querySelector('#volume')  
+function volume_change(){
+    songAudio.volume = recent_volume.value / 100;
+}
+
+
+
+// --------danh sách nhạc----------
+
+const showMusic = document.querySelector("#fa-list")
+const closeMusic = document.querySelector(".fa-close")
+const listMusic = document.querySelector(".list-music")
+const ulMusic = wrapper.querySelector("ul")
+
+showMusic.addEventListener("click", function(){
+    listMusic.classList.add("show")
+})
+closeMusic.addEventListener("click", function(){
+    listMusic.classList.remove("show")
+})
+
+for (let i=0; i< allMusic.length; i++){
+    let liMusic = `<li Musicindex="${i}">
+                        <div class="song-title">
+                            <span>${allMusic[i].name}</span>
+                            <p>${allMusic[i].artist}</p>
+                        </div>
+                        <audio class="${allMusic[i].idMusic}" src="${allMusic[i].src}"></audio>
+                        <span id="${allMusic[i].idMusic}" class="audio-duration">3:04</span>
+                    </li>`;
+    ulMusic.insertAdjacentHTML("beforeend", liMusic);
+    let liAudioDuration = ulMusic.querySelector(`#${allMusic[i].idMusic}`)
+    let liAudioTag = ulMusic.querySelector(`.${allMusic[i].idMusic}`)
+
+// -------hiển thị thời gian bài hát------------
+    liAudioTag.addEventListener("loadeddata", ()=>{
+        let audioDuration = liAudioTag.duration;
+        let totalMusicmin = Math.floor(audioDuration/60);
+        let totalMusicsec = Math.floor(audioDuration%60);
+        if(totalMusicsec < 10){
+            totalMusicsec = `0${totalMusicsec}`;
+        }
+        liAudioDuration.innerText = `${totalMusicmin}:${totalMusicsec}`;
+    });
+
+}
+// -----------------------------------------------
+
+const allLiMusic = ulMusic.querySelectorAll("li");
+function playLiMusic(){
+    for (let j = 0; j < allLiMusic.length; j++) {
+        if(allLiMusic[j].classList.contains("playMusic")){
+            allLiMusic[j].classList.remove("playMusic")
+        }
+        if(allLiMusic[j].getAttribute("Musicindex") == musicIndex){
+            allLiMusic[j].classList.add("playMusic");
+        }
+        allLiMusic[j].setAttribute("onclick", "clicked(this)");
+    }
+}
+
+function clicked(index){
+    let getLiIndex = index.getAttribute("Musicindex");
+    musicIndex = getLiIndex;
+    loadMusic(musicIndex);
+    playsong();
+    playLiMusic();
+}
+
